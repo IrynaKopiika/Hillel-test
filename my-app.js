a13d73fd7d1a7a3baf304c941097a0ed822ab80d
kopiikaIryna <131687628+IrynaKopiika@users.noreply.github.com>
2023-05-09 21:54:51 +0300

    Homework js

--- my-app.js
+++ my-app.js
@@ -1,4 +1,53 @@
-console.log('one')
-console.log('two')
-console.log('one')
-console.log('one')
+// let year = 2023;
+// if (year === 2023) {
+//     console.log("Present")
+// }
+// else if (year < 2023) {
+//     console.log ("Past")
+// }
+// else if (year > 2023) {
+//     console.log("Future")
+// }
+
+
+// let yearsWorked = 4;
+// let baseSalary = 3000;
+//
+// let increasedSalary;
+// if (yearsWorked >= 1 && yearsWorked <= 3) {
+//     increasedSalary = baseSalary + (baseSalary * 0.1);
+// } else if (yearsWorked >= 3) {
+//     increasedSalary = baseSalary + (baseSalary * 0.2);
+// }
+//
+// let finalSalary;
+// if (increasedSalary < 4000) {
+//     finalSalary = increasedSalary + 1000;
+// } else {
+//     finalSalary = increasedSalary + 500;
+// }
+//
+// console.log("Зарплата:", finalSalary);
+
+// }
+
+ // let age = 12;
+ // if (age < 0) {
+ //     console.log ("Wrong age");
+ // }
+ // else if (age <= 18) {
+ //     console.log("You are not yet of legal age");
+ // }
+ // else if (age > 18) {
+ //     console.log("You have become an adult");
+ // }
+
+// let a = 5
+// let b = 3
+// if (a % b === 0) {
+//     console.log("Sharing", a / b);
+// }
+// else if (a % b !== 0){
+//     console.log("Divide by the remainder", a % b);
+//
+// }
